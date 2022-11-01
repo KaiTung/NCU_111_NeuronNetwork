7c4d53e108a0e56b59d0ec36a9c03bbfcc79e59a
KaiTung <allen01105@gmail.com>
2022-11-01 23:42:36 +0800

    11/1 update

--- HW2/main.py
+++ HW2/main.py
@@ -9,6 +9,24 @@ import math
 import sys
 import os
 
+class car(object):
+    def __init__(self,x = 0,y = 0,theta = 0,phi = 90,b = 3):
+        self.x = x
+        self.y = y
+        self.theta = theta  # -40 < theta < 40
+        self.phi = phi   # -90 < phi < 270
+        self.b = b      #car length
+        
+    def car_move(self,theta):
+        # 更新 x,y,phi
+        # 要先把Degree轉radian
+        radian_theta = math.radians(theta)
+        radian_phi = math.radians(self.phi)
+        self.x = self.x + math.cos(radian_phi + radian_theta) + math.sin(radian_theta) * math.sin(radian_phi)
+        self.y = self.y + math.sin(radian_phi + radian_theta) - math.sin(radian_theta) * math.cos(radian_phi)
+        self.phi = self.phi - (math.asin(2 * math.sin(radian_theta)/self.b) * (180.0 / math.pi)) # radian to degree
+    
+        
 class MyGUI(QtWidgets.QMainWindow):
 
     def __init__(self):
@@ -21,6 +39,8 @@ class MyGUI(QtWidgets.QMainWindow):
         with open('軌道座標點.txt','r') as f:
             for i in f.read().splitlines():
                 self.map.append(np.array(i.split(',')).astype(int))
+        #畫布宣告一次就好
+        self.fig = plt.figure()
         self.plot_new_graph(0,0,90)
         # 取得label_image
         self.label_image = self.findChild(QtWidgets.QLabel,"label_image")
@@ -29,21 +49,23 @@ class MyGUI(QtWidgets.QMainWindow):
         self.label_image.show()
         # 宣告一個RBFN model
         self.RBFN = MyRBFN(h_layers = 20,sigma = 1)
+        # 宣告一個Mycar
+        self.Mycar = car()
+        # 連結按鈕事件
+        self.pushButton_GO = self.findChild(QtWidgets.QPushButton,"pushButton_GO")
+        self.pushButton_GO.clicked.connect(self.auto_car_move)
         # Show the GUI
         self.show()
 
     
-    def formula(x,y,theta,phi):
+    def auto_car_move(self):
+        self.Mycar
+        self.Mycar.car_move(0)
+        self.plot_new_graph(self.Mycar.x,self.Mycar.y,self.Mycar.phi)
         
-        new_x = x + math.cos(phi + theta) + math.sin(theta) * math.sin(phi)
-        new_y = y + math.sin(phi + theta) - math.sin(theta) * math.cos(phi)
-        new_phi = phi - math.asin(2*math.sin(theta)/3)
+    def plot_new_graph(self,x,y,phi):
         
-        return new_x,new_y,new_phi
-        
-    def plot_new_graph(self,x,y,degree):
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
+        ax = self.fig.add_subplot(111)
         #畫起點
         plt.plot((-6,6),(0,0),c = "r")
         #畫終點線
@@ -59,15 +81,15 @@ class MyGUI(QtWidgets.QMainWindow):
         ax.add_patch(circle)
         #感測器線條
         length = 10
-        degree -= 45
-        for i in range(0,3):
-            endy = y + length * math.sin(math.radians(degree))
-            endx = length * math.cos(math.radians(degree))
-            plt.plot((x,endx),(y,endy),c='r')
-            degree += 45
+        endy = y + length * math.sin(math.radians(phi))
+        endx = length * math.cos(math.radians(phi))
+        plt.plot([x,endx],[y,endy],c = 'r')
         plt.xlim([-20,40])
         plt.ylim([-10,55])
         plt.savefig("pic")
+        #更新畫面
+        self.label_image.setPixmap(QtGui.QPixmap("pic.png"))
+        plt.clf()
         
 
 
