19400302fc5aad9d0955b84009756c998a711a22
KaiTung <allen01105@gmail.com>
2022-12-08 17:41:28 +0800

    update HW3

--- HW3/MyHopfidle.py
+++ HW3/MyHopfidle.py
@@ -1,46 +1,124 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import os
 
 class MyHopifield():
-    def init(x):
+    def init(self):
         self.w = None
-        self.x = x
-        self.mode = "basic"
-    
-    def change_mode(self):
-        if self.mode == "basic":
-            self.mode = "bonus"
-        else:
-            self.mode = "basic"
-    
-    def update_P_N(self):
-        if self.mode == "basic":
-            self.P = 9*12
-            self.N = 3
-        elif self.mode == "bonus":
-            self.P = 10*10
-            self.N = 15
-        
-    def calculate_w():
-        self.w = (np.dot(self.x,self.x.T) - self.N) / self.P
-        
 
-def data_process():
-    path_to_file = "Basic_Training.txt"
-    
+    def read_data(self,x):
+        self.x = x
+        self.N = self.x.shape[0]
+        self.P = self.x.shape[1]
+        if self.P == 100:
+            self.H = 10
+            self.W = 10
+        if self.P == 108:
+            self.H = 9
+            self.W = 12
+        
+    def calculate_w(self):
+        self.w = (np.dot(self.x.T,self.x) - self.N*np.identity(self.P)) / self.P
+        self.theta = np.dot(self.w,[1]*self.P)
+
+    def cmp(self,v1,v2):
+        dif = 0
+        for i in range(len(v1)):
+            if v1[i] != v2[i]:
+                dif+=1
+        if dif > 0:
+            return False
+        else:
+            return True
+
+    def think(self,x):
+        c = 0
+        sign_ans = x
+        while 1:
+            if c >= 10:
+                break
+            temp = sign_ans
+
+            ans = np.dot(x,self.w) - self.theta
+            sign_ans = np.sign(ans)
+
+            for i in range(len(sign_ans)):
+                if sign_ans[i] == 0:
+                    sign_ans[i] = x[i]
+            
+            if self.cmp(sign_ans,temp):
+                break
+            
+            c+=1
+        return np.array(sign_ans)
+
+    def save_img(self,x,type):
+        for i in range(x.shape[0]):
+            plt.axis('off')
+            plt.margins(0)
+            plt.subplots_adjust(top=1,bottom=1,right=1,left=1,hspace=1,wspace=1)
+            plt.imshow(x[i],cmap='Greys')
+            if type == "train":
+                plt.savefig("train_img_{}.png".format(i))
+            if type == "before":
+                plt.savefig("before_img_{}.png".format(i))
+            if type == "after":
+                plt.savefig("after_img_{}.png".format(i))
+            
+
+
+def data_preprocess(path_to_file):    
     temp = []
     temp2 = []
-    for line in open(path_to_file,'r'):
-        
-        if line != "\n":
-            temp.append(line)
-        else:
-            temp2.append(temp)
-            temp.clear()
-    print(temp2)
+    with open(path_to_file,'r') as f:
+        for line in f.readlines():
+            re_line = line.replace('\n','') #換行處理掉
+            if re_line != '':
+                for num in re_line:
+                    if num == ' ':
+                        temp.append(-1) #空白補-1
+                    else:
+                        temp.append(int(num)) 
+            else:
+                temp2.append(temp[:])
+                temp.clear()
+    temp2.append(temp[:])
+    return np.array(temp2)
 
+def delete_img():
+    i=0
+    while 1:
+        try:
+            os.remove("train_img_{}.png".format(i))
+            os.remove("before_img_{}.png".format(i))
+            os.remove("after_img_{}.png".format(i))
+        except Exception as e:
+            print(e)
+            break
+        i+=1
 
 def main():
-    data_process()
+    hopifield = MyHopifield()
+
+    x = data_preprocess("Basic_Training.txt")
+    # x = np.array([[1,-1,1],[-1,1,-1]])
+    hopifield.read_data(x)
+
+    hopifield.calculate_w()
+
+    test_x= []
+    x2 = data_preprocess("Basic_Testing.txt")
+    for xx in x2:
+        test_x.append(xx.reshape(hopifield.W,hopifield.H))
+    hopifield.save_img(np.array(test_x),type="before")
+
+    think_x2=[]
+    for xx in x2:
+        output = hopifield.think(xx).reshape(hopifield.W,hopifield.H)
+        think_x2.append(output)
     
+    hopifield.save_img(np.array(think_x2),type="after")
+        
+
 if __name__ == "__main__":
     main()
--- HW3/main.py
+++ HW3/main.py
@@ -9,16 +9,47 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = uic.loadUi('myGUI.ui', self)
-                
+        self.ui = uic.loadUi('MyGUI.ui', self)
+        self.Hopefield = MyHopifield()
+        
+        # 取得label_image
+        self.label_img_before = self.findChild(QtWidgets.QLabel,"label_img_before")
+        self.label_img_before.setScaledContents(True)
+        self.label_img_after = self.findChild(QtWidgets.QLabel,"label_img_after")
+        self.label_img_after.setScaledContents(True)
+
+        self.label_img_before.setPixmap(QtGui.QPixmap("before_img_0.png"))
+        self.label_img_after.setPixmap(QtGui.QPixmap("after_img_0.png"))
+
+        #conbobox
+        self.comboBox = self.findChild(QtWidgets.QComboBox,"comboBox")
+
+        self.pushButton_OK = self.findChild(QtWidgets.QPushButton,"pushButton_OK")
+        self.pushButton_OK.clicked.connect(self.train)
+        
         # Show the GUI
         self.show()
+
+    def train(self):
+        return
+
+def delete_img():
+    i=0
+    while 1:
+        try:
+            os.remove("train_img_{}.png".format(i))
+            os.remove("before_img_{}.png".format(i))
+            os.remove("after_img_{}.png".format(i))
+        except Exception as e:
+            print(e)
+            break
+        i+=1
 
 def main():
     app = QtWidgets.QApplication(sys.argv) # Create an instance of QtWidgets.QApplication
     window = MainWindow() # Create an instance of our class
     app.exec_() # Start the application
-
+            
 
 if __name__ == '__main__':
     main()
